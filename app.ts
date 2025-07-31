361fb20976dcda1a886b12fd9388749f5599906a
kasper666g <64731263+kasper666g@users.noreply.github.com>
2025-07-31 18:07:45 +0300

    update

--- app.ts
+++ app.ts
@@ -1,76 +1,49 @@
-console.log("Hello World!");
-type TVehicle = {
-  model: string;
-  color: string;
-  year: number;
-  power: number;
-};
-const vehicle: TVehicle = {
-  model: "Boring generic vehicle",
-  color: "Red",
-  year: 1993,
-  power: 60
-};
-console.log("Generic vehicle:", vehicle);
 interface IVehicle {
-  model: string;
-  color: string;
-  year: number;
-  power: number;
-}
-interface ICar extends IVehicle {
-  bodyType: string;
-  wheelCount: number;
-}
-interface IBoat extends IVehicle {
-  draft: number;
-}
-
-interface IPlane extends IVehicle {
-  wingspan: number;
-}
-
-const car: ICar = {
-  model: "Ford focus",
-  color: "Green",
-  year: 2016,
-  power: 150,
-  bodyType: "Hatchback",
-  wheelCount: 4
-};
-const plane: IPlane = {
-  model: "Boeing 777",
-  color: "White",
-  year: 2020,
-  power: 170000,
-  wingspan: 65
-};
-const boat: IBoat = {
-  model: "Bella",
-  color: "Black",
-  year: 2022,
-  power: 100,
-  draft: 0.42
-};
-console.log("Car instance:", car);
-console.log("Plane instance:", plane);
-console.log("Boat instance:", boat);
-
-class VehicleService<T> {
-  private items: T[] = [];
-
-  add(item: T): void {
-    this.items.push(item);
+    model: string;
+    color: string;
+    year: number;
+    power: number;
   }
-
-  list(): T[] {
-    return this.items;
+  
+  interface ICar extends IVehicle {
+    bodyType: string;
+    wheelCount: number;
   }
-}
-
-const auto = new VehicleService<ICar>();
-const vene = new VehicleService<IBoat>();
-auto.add(car);
-vene.add(boat);
-console.log("Cars in service:", auto.list());
-console.log("Boats in service:", vene.list());
+  
+  interface IBoat extends IVehicle {
+    draft: number;
+  }
+  
+  interface IPlane extends IVehicle {
+    wingspan: number;
+  }
+  
+  const car: ICar = {
+    model: "Ford focus",
+    color: "Green",
+    year: 2016,
+    power: 150,
+    bodyType: "Hatchback",
+    wheelCount: 4
+  };
+  
+  const plane: IPlane = {
+    model: "Boeing 777",
+    color: "White",
+    year: 2020,
+    power: 170000,
+    wingspan: 65
+  };
+  
+  const boat: IBoat = {
+    model: "Bella",
+    color: "Black",
+    year: 2022,
+    power: 100,
+    draft: 0.42
+  };
+  
+  console.log(car);
+  console.log(plane);
+  console.log(boat);
+  

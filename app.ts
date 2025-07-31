510b39eb1449af3194b6189424e308209a209db2
kasper666g <64731263+kasper666g@users.noreply.github.com>
2025-07-31 18:37:24 +0300

    small update

--- app.ts
+++ app.ts
@@ -1,18 +1,27 @@
-
 console.log("Hello World!");
-
+type TVehicle = {
+  model: string;
+  color: string;
+  year: number;
+  power: number;
+};
+const vehicle: TVehicle = {
+  model: "Boring generic vehicle",
+  color: "Red",
+  year: 1993,
+  power: 60
+};
+console.log("Generic vehicle:", vehicle);
 interface IVehicle {
   model: string;
   color: string;
   year: number;
   power: number;
 }
-
 interface ICar extends IVehicle {
   bodyType: string;
   wheelCount: number;
 }
-
 interface IBoat extends IVehicle {
   draft: number;
 }
@@ -29,7 +38,6 @@ const car: ICar = {
   bodyType: "Hatchback",
   wheelCount: 4
 };
-
 const plane: IPlane = {
   model: "Boeing 777",
   color: "White",
@@ -37,7 +45,6 @@ const plane: IPlane = {
   power: 170000,
   wingspan: 65
 };
-
 const boat: IBoat = {
   model: "Bella",
   color: "Black",
@@ -45,9 +52,25 @@ const boat: IBoat = {
   power: 100,
   draft: 0.42
 };
+console.log("Car instance:", car);
+console.log("Plane instance:", plane);
+console.log("Boat instance:", boat);
 
-console.log(car);
-console.log(plane);
-console.log(boat);
+class VehicleService<T> {
+  private items: T[] = [];
 
-  
+  add(item: T): void {
+    this.items.push(item);
+  }
+
+  list(): T[] {
+    return this.items;
+  }
+}
+
+const auto = new VehicleService<ICar>();
+const vene = new VehicleService<IBoat>();
+auto.add(car);
+vene.add(boat);
+console.log("Cars in service:", auto.list());
+console.log("Boats in service:", vene.list());
